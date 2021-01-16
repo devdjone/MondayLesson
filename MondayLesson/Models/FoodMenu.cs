ebfd8619ac9bdf8422ccb4fd781fd6214b49345d
voytek <cssdeveloper@hotmail.com>
2021-01-16 15:15:05 -0700

    adding food menu

---
+++ MondayLesson/Models/FoodMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MondayLesson.Models
+{
+    public class FoodMenu
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
